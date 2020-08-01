fe50c9dbeb4bbe3c1f7275dc16b5d6f3f26e836b
NilaCk <NilaCkGames@gmail.com>
2020-08-01 19:41:49 +0300

    Made Enemy Health

---
+++ Assets/NilaCk/Zombies.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Zombies : MonoBehaviour
+{
+    public float maxHealth = 100;
+    float currentHealth;
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    public void TakeDamage(int damageAmount)
+    {
+        currentHealth -= damageAmount;
+        if(currentHealth <= 0)
+        {
+
+        }
+    }
+}
