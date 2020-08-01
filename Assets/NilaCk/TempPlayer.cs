02f4d3d1f186581cce44317f05629d7c2d3ae226
NilaCk <NilaCkGames@gmail.com>
2020-08-01 18:49:08 +0300

    Made Not Finished A* Pathfinding

---
+++ Assets/NilaCk/TempPlayer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TempPlayer : MonoBehaviour
+{
+    //Movement Variables
+    public float speed = 8.5f;
+    Rigidbody2D rb;
+    public Vector2 movement;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
+
+        if (Input.GetKeyDown(KeyCode.R))
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
