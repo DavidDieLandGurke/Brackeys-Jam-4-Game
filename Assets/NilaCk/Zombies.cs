50db61a15e96fd15575a4dd600a6e2ec39348e44
DavidDieLandGurke <61883039+DavidDieLandGurke@users.noreply.github.com>
2020-08-02 17:03:01 +0200

    Addes enemy health bar

--- Assets/NilaCk/Zombies.cs
+++ Assets/NilaCk/Zombies.cs
@@ -1,24 +1,40 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Zombies : MonoBehaviour
 {
     public float maxHealth = 100;
     float currentHealth;
 
+    public Slider healthSlider;
+    public GameObject sliderCanvas;
 
+    public Gradient gradient;
+    public Image fill;
 
     void Start()
     {
         currentHealth = maxHealth;
+        healthSlider.maxValue = maxHealth;
+        fill.color = gradient.Evaluate(1);
+    }
+
+    void Update()
+    {
+        healthSlider.value = currentHealth;
+        sliderCanvas.transform.position = transform.position;
+        
     }
 
     public void TakeDamage(int damageAmount)
     {
         currentHealth -= damageAmount;
-        if(currentHealth <= 0)
+        fill.color = gradient.Evaluate(healthSlider.normalizedValue);
+        if (currentHealth <= 0)
         {
+            Destroy(sliderCanvas);
             Destroy(gameObject);
         }
     }
