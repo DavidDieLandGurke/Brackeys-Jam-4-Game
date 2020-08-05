3ddbb83f584016bdc1c999a7942ee6e56286492b
NilaCk <NilaCkGames@gmail.com>
2020-08-06 02:41:15 +0300

    Made a tutorial

---
+++ Assets/NilaCk/Scripts/TutorialManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TutorialManager : MonoBehaviour
+{
+    public GameObject[] texts;
+    int textsIndex;
+
+    void Update()
+    {
+        for (int i = 0; i < texts.Length; i++)
+        {
+            texts[i].SetActive(i == textsIndex);
+        }
+
+
+
+        //if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.Space)
+        //        || Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.D) ||
+        //        Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.DownArrow)
+        //        || Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.RightArrow))
+        //{
+
+        //      if (textsIndex == 0)
+        //    {
+        //        textsIndex++;
+        //    }
+        //}
+        //else if (Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Space))
+        //{
+        //    if (textsIndex == 1)
+        //                textsIndex++;
+        //}
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (textsIndex < 3)
+                textsIndex++;
+        }
+        else if (Input.GetKeyDown(KeyCode.Space) || (Input.GetMouseButtonDown(0)))
+        {
+            if (textsIndex == 1)
+                textsIndex++;
+        }
+    }
+
+    //void OnCollisionEnter2D(Collision2D collision)
+    //{
+    //    if (textsIndex == 3 && collision.gameObject.CompareTag("Finish"))
+    //    {
+    //        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    //    }
+    //}
+}
