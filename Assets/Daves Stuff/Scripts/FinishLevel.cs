2999368cd43343696c9b7daab866b48c8d65a72a
DavidDieLandGurke <61883039+DavidDieLandGurke@users.noreply.github.com>
2020-08-07 13:09:46 +0200

    made system that made you need to defeat every enemy

---
+++ Assets/Daves Stuff/Scripts/FinishLevel.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FinishLevel : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(FindObjectOfType<Enemies>() == null)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+}
