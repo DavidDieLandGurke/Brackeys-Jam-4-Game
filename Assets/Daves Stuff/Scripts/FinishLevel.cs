ae16e11453a4a79ba75670cb5f519721cfb6585d
DavidDieLandGurke <61883039+DavidDieLandGurke@users.noreply.github.com>
2020-08-07 16:40:04 +0200

    som stuff

--- Assets/Daves Stuff/Scripts/FinishLevel.cs
+++ Assets/Daves Stuff/Scripts/FinishLevel.cs
@@ -5,11 +5,21 @@ using UnityEngine.SceneManagement;
 
 public class FinishLevel : MonoBehaviour
 {
+    public GameObject warningText;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(FindObjectOfType<Enemies>() == null)
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         }
+        else
+        {
+            warningText.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        warningText.SetActive(false);
     }
 }
